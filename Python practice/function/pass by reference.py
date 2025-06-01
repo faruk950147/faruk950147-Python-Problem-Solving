7d2bb5c8a6f92995df33361569b2444090d5595a
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-01 12:43:31 +0600

    Uploaded all files

---
+++ Python practice/function/pass by reference.py
@@ -0,0 +1,15 @@
+def pass_by_reference(lst):
+    lst[0] = lst[0] + 1 # pass by reference
+    return lst
+
+
+a = [10]
+print(pass_by_reference(a))
+print(a)
+
+def change_list(lst):
+    lst.append(100)
+
+my_list = [1, 2, 3]
+change_list(my_list)
+print(my_list)  # Output: [1, 2, 3, 100]
---
+++ Python practice/function/pass by value.py
@@ -0,0 +1,8 @@
+def pass_by_value(x):
+    x = x + 1 # pass by value
+    return x    
+
+
+a = 10
+print(pass_by_value(a))
+print(a)
