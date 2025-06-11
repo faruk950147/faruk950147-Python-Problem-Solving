14f19dc33c579169066cde01e102ab318d662578
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-11 07:50:44 +0600

    Uploaded all files

--- Python practice/threead/threatening.py
+++ Python practice/threead/threatening.py
@@ -1,4 +1,24 @@
 import threading
 
-current_thread = threading.current_thread().name
-print(current_thread) 
+# current_thread = threading.current_thread().name
+# print(current_thread) 
+
+# def display(n):
+#     for i in range(10):
+#         print(n)
+        
+# thread1 = threading.Thread(target=display, args=("Hello",))
+# thread2 = threading.Thread(target=display, args=("World",))
+
+# thread1.start()
+# thread2.start()
+
+def display(n, msg):
+    for i in range(10):
+        print(n, msg)
+        
+thread1 = threading.Thread(target=display, args=("Hello", "Welcome"))
+thread2 = threading.Thread(target=display, args=("World", "Welcome"))
+
+thread1.start()
+thread2.start()
