b6d7f425bd931b68bac703bb85af7616b1b31ec6
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-15 21:07:30 +0600

    Uploaded all files

--- Python practice/opp/account.py
+++ Python practice/opp/account.py
@@ -1,3 +1,30 @@
+# class Account:
+#     def __init__(self, owner, account_number, balance=0):
+#         self.owner = owner
+#         self.account_number = account_number
+#         self.balance = balance
+    
+#     def deposit(self, amount):
+#         self.balance += amount
+#         print(f"Deposited {amount}. Total balance: {self.balance}")
+    
+#     def withdraw(self, amount):
+#         if amount > self.balance:
+#             print("Insufficient balance")
+#         else:
+#             self.balance -= amount
+#             print(f"Withdrawn {amount}. Total balance: {self.balance}")
+    
+#     def get_balance(self):
+#         print(f"Balance for {self.owner}: {self.balance}")
+
+# if __name__ == "__main__":
+#     account = Account("John", "123456789", 1000)
+#     account.get_balance()
+#     account.deposit(500)
+#     account.withdraw(200)
+#     account.get_balance()
+    
 class Account:
     def __init__(self, owner, account_number, balance=0):
         self.owner = owner
@@ -6,21 +33,21 @@ class Account:
     
     def deposit(self, amount):
         self.balance += amount
-        print(f"Deposited {amount}. Total balance: {self.balance}")
+        print(f"Deposited {amount}. Total balance: {self.get_balance()}")
     
     def withdraw(self, amount):
-        if amount > self.balance:
+        if amount > self.get_balance():
             print("Insufficient balance")
         else:
             self.balance -= amount
-            print(f"Withdrawn {amount}. Total balance: {self.balance}")
+            print(f"Withdrawn {amount}. Total balance: {self.get_balance()}")
     
-    def check_balance(self):
-        print(f"Balance for {self.owner}: {self.balance}")
+    def get_balance(self):
+        return self.balance
 
 if __name__ == "__main__":
     account = Account("John", "123456789", 1000)
-    account.check_balance()
+    account.get_balance()
     account.deposit(500)
     account.withdraw(200)
-    account.check_balance()
+    account.get_balance()
