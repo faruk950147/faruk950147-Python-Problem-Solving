67fda2a45b0296880c9251a21ad116b6670ede26
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-19 12:46:14 +0600

    Uploaded all files

--- Python practice/regular expreshion/regular Ex1.py
+++ Python practice/regular expreshion/regular Ex1.py
@@ -1,3 +1,17 @@
-s = 'Afganistan, America, Bangladesh, Canada, Demark, Sweden, Greenland, Iceland, Netherlands, New Zealand, Switzerland'
+import re
+
+s = 'Afghanistan, America, Bangladesh, Canada, Demark, Sweden, Greenland, Iceland, Netherlands, New Zealand, Switzerland'
 countries = s.split(',')
-print(countries)
+# li  = [item for item in countries if item.endswith('land')]
+# print(li)
+
+# li = [item for item in countries if item.startswith('A')]
+# print(li)
+
+# li = [item for item in countries if item.startswith('A') or item.endswith('land')]
+# print(li)
+# li = re.findall(r'\w+lands*', s)
+# print(li)
+
+li = re.search(r'\w+lands*', s)
+print(li.group())
