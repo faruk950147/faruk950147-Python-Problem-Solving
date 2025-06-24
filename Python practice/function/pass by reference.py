6389c6dbafeb5edf220f2ee456a2065e6e362a1c
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-24 19:37:16 +0600

    Uploaded all files

--- Python practice/function/pass by reference.py
+++ Python practice/function/pass by reference.py
@@ -1,42 +1,46 @@
-# # no change in original list and same memory location
-# pass by reference
+# When a variable is passed to a function, its reference (actual address) is sent.
+# As a result, any changes made inside the function also affect the original variable outside.
 # যখন ভ্যারিয়েবল function-এ পাঠানো হয় তার রেফারেন্স (মূল address) পাঠানো হয়।
 
 # ফলে function এর ভেতর যা পরিবর্তন হয়, তা বাইরে মূল ভ্যারিয়েবলেও প্রভাব ফেলে।
 
-# def pass_by_reference(x):
-#     x[0] = 20
-#     print("Inside function:", x, id(x))
+def pass_by_reference(x):
+    print(f"{x} Before memory location inside function:", x, id(x))
+    x[0] = 20
+    print(f"{x} Inside function x:", x)
+    print(f"{x} After memory location inside function x:", x, id(x))
+    return x
 
-# x = [10]  # mutable object (list)
-# print("Before modification:", x, id(x))
-# pass_by_reference(x)
-# print("After modification:", x, id(x))
+x = [10]  # mutable object (list)
+print(f"{x} Before modification x:", x, id(x))
+pass_by_reference(x)
+print(f"{x} After modification x:", x, id(x))
 
 # def pass_by_reference(lst):
-#     # lst[0] = lst[0] + 1
-#     lst[0] += 1 # pass by reference
-    
-#     print("Inside function:", lst, id(lst))
+#     print(f"{lst} Before memory location inside function:", lst, id(lst))
+#     lst[0] = lst[0] + 1
+#     # lst[0] += 1 # pass by reference    
+#     print(f"{lst} Inside function lst:", lst)
+#     print(f"{lst} After memory location inside function lst:", lst, id(lst))
 #     return lst
 
 
 # li = [10]
-# print("Before modification:", li, id(li))
+# print(f"{li} Before modification li:", li, id(li))
 # pass_by_reference(li)
-# print("After modification:", li, id(li))
+# print(f"{li} After modification li:", li, id(li))
 
 
-def change_list(lst):
-    print("before memory location:", lst, id(lst))
-    lst.append(100)
-    print("Inside function:", lst, id(lst))
-    print("after memory location:", lst, id(lst))
-    return lst
+# def change_list(lst):
+#     print(f"{lst} before memory location inside function:", lst, id(lst))   
+#     lst.append(100)
+#     print(f"{lst} Inside function lst:", lst)
+#     print(f"{lst} after memory location inside function lst:", lst, id(lst))
+#     return lst
 
-list1 = [1, 2, 3]
-print("Before modification:", list1, id(list1))
-change_list(list1)
-print("After modification:", list1, id(list1))  # Output: [1, 2, 3, 100]
+# list1 = [1, 2, 3]
+# print(f"{list1} Before modification list1:", list1, id(list1))
+# change_list(list1)
+# print(f"{list1} After modification list1:", list1, id(list1))  # Output: [1, 2, 3, 100]
 
 
--- Python practice/function/pass by value.py
+++ Python practice/function/pass by value.py
@@ -1,30 +1,32 @@
-# change in original list and same memory location
+# When a variable is passed to a function, a copy of it is sent.
+# The original variable remains unaffected. 
+
 # যখন কোনো ভ্যারিয়েবল function-এ পাঠানো হয় তার কপি (copy) পাঠানো হয়।
 # মূল ভ্যারিয়েবল এর উপর কোনো প্রভাব পড়ে না।
 
-# def pass_by_value(x):
-#     print("before memory location:", x, id(x))
-#     # x = x + 1 # pass by value
-#     x += 1 # pass by value   
-#     print("Inside function:", x)
-#     print("after memory location:", x, id(x))
-#     return x    
+def pass_by_value(x):
+    print(f"{x} Before memory location inside function:", x, id(x))
+    x = x + 1 # pass by value
+    print(f"{x} Inside function x:", x)
+    print(f"{x} After memory location inside function x:", x, id(x))
+    return x    
 
 
-# a = 10
-# print("Before modification:", a, id(a))
-# pass_by_value(a)
-# print("After modification:", a, id(a))
+a = 10
+print(f"{a} Before modification a:", a, id(a))
+pass_by_value(a)
+print(f"{a} After modification a:", a, id(a))
 
 # def change_value(x):
-#     x = x + 10
-    
-#     print("Inside function:", x)
+#     print(f"{x} before memory location inside function:", x, id(x))
+#     x += 10
+#     print(f"{x} Inside function x:", x)
+#     print(f"{x} after memory location inside function x:", x, id(x))
 #     return x
 
 # a = 5
-# print("Before modification:", a, id(a))
+# print(f"{a} Before modification a:", a, id(a))
 # change_value(a)
-# print("After modification:", a, id(a))
+# print(f"{a} After modification a:", a, id(a))
 
 
