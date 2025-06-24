ecbcd629c1f925f4fabd6f461a63115ef71a2c63
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-24 13:16:18 +0600

    Uploaded all files]

--- Python practice/function/pass by reference.py
+++ Python practice/function/pass by reference.py
@@ -1,16 +1,38 @@
-# no change in original list and same memory location
-def pass_by_reference(lst):
-    lst[0] = lst[0] + 1 # pass by reference
-    return lst
+# # no change in original list and same memory location
+# pass by reference
+# যখন ভ্যারিয়েবল function-এ পাঠানো হয় তার রেফারেন্স (মূল address) পাঠানো হয়।
+
+# ফলে function এর ভেতর যা পরিবর্তন হয়, তা বাইরে মূল ভ্যারিয়েবলেও প্রভাব ফেলে।
+
+# def pass_by_reference(x):
+#     x[0] = 20
+#     print("Inside function:", x, id(x))
+
+# x = [10]  # mutable object (list)
+# print("Before modification:", x, id(x))
+# pass_by_reference(x)
+# print("After modification:", x, id(x))
+
+# def pass_by_reference(lst):
+#     # lst[0] = lst[0] + 1
+#     lst[0] += 1 # pass by reference
+    
+#     print("Inside function:", lst, id(lst))
+#     return lst
 
 
-a = [10]
-print(pass_by_reference(a))
-print(a)
+# li = [10]
+# print("Before modification:", li, id(li))
+# pass_by_reference(li)
+# print("After modification:", li, id(li))
+
 
 def change_list(lst):
     lst.append(100)
+    print("Inside function:", lst, id(lst))
+    return lst
 
 my_list = [1, 2, 3]
+print("Before modification:", my_list, id(my_list))
 change_list(my_list)
-print(my_list)  # Output: [1, 2, 3, 100]
+print("After modification:", my_list, id(my_list))  # Output: [1, 2, 3, 100]
