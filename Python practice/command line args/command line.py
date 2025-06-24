2dc24de0956be1de2b504e1a82d60d9f41788317
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-24 20:05:14 +0600

    Uploaded all files

---
+++ Python practice/command line args/command line.py
@@ -0,0 +1,17 @@
+import sys
+
+# print("Script name:", sys.argv[0])
+# print("First argument:", sys.argv[1])
+# print("Second argument:", sys.argv[2])
+
+print("Script name:", sys.argv[0])
+
+if len(sys.argv) > 1:
+    print("First argument:", sys.argv[1])
+else:
+    print("First argument not provided.")
+
+if len(sys.argv) > 2:
+    print("Second argument:", sys.argv[2])
+else:
+    print("Second argument not provided.")
