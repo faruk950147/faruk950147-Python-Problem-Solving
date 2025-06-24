899f4f20a923ce1701b35a020d6a39e9dbc211f7
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-24 13:32:24 +0600

    Uploaded all files

--- Python practice/function/pass by value.py
+++ Python practice/function/pass by value.py
@@ -2,22 +2,27 @@
 # যখন কোনো ভ্যারিয়েবল function-এ পাঠানো হয় তার কপি (copy) পাঠানো হয়।
 # মূল ভ্যারিয়েবল এর উপর কোনো প্রভাব পড়ে না।
 
-# def pass_by_value(x):
-#     x = x + 1 # pass by value   
-#     print("Inside function:", x, id(x))
-#     return x    
+def pass_by_value(x):
+    # x = x + 1 # pass by value
+    x += 1 # pass by value   
+    print("before memory location:", id(x))
+    print("Inside function:", x)
+    print("after memory location:", id(x))
+    return x    
 
 
-# a = 10
-# print("Before modification:", a, id(a))
-# print("After modification:", a, id(a))
-
-def change_value(x):
-    x = x + 10
-    print("Inside function:", x, id(x))
-    return x
-
-a = 5
+a = 10
 print("Before modification:", a, id(a))
-change_value(a)
+pass_by_value(a)
 print("After modification:", a, id(a))
+
+# def change_value(x):
+#     x = x + 10
+    
+#     print("Inside function:", x)
+#     return x
+
+# a = 5
+# print("Before modification:", a, id(a))
+# change_value(a)
+# print("After modification:", a, id(a))
