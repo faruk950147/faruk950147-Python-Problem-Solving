321692c9504e5f6fd79d4c0a5dc98295b5b345af
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-25 22:07:06 +0600

    Uploaded all files

---
+++ Python practice/datatype/Literal.py
@@ -0,0 +1,18 @@
+# Literal মানে হচ্ছে সরাসরি কোনো ভ্যালু লিখে দেওয়া।
+
+# মানে Python-এ যখন তুমি ভেরিয়েবল না দিয়ে সরাসরি ভ্যালু দাও, তখন সেটা Literal।
+
+x = 10        # Integer literal
+y = 3.14      # Floating point literal
+z = 0b1010    # Binary literal
+a = 0o12      # Octal literal
+b = 0xA       # Hexadecimal literal
+name = "Omar"         # String literal
+message = 'Hello'     # String literal
+status = True
+is_valid = False
+data = None   # Special literal
+my_list = [1, 2, 3]               # List literal
+my_tuple = (1, 2, 3)              # Tuple literal
+my_dict = {"name": "Omar"}        # Dictionary literal
+my_set = {1, 2, 3}                # Set literal
