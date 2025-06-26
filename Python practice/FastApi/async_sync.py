3c6722040256242f93fb62f4750f9e49839ebc95
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-26 18:39:17 +0600

    Uploaded all files

---
+++ Python practice/FastApi/async_sync.py
@@ -0,0 +1,10 @@
+# Async is a non-blocking call and it will not block the execution of the program
+#Non-blocking Waiting for the task to complete
+
+# scope  HTTP, WebSocket, path, headers, query_string, client, server, extensions
+# receive 
+# send
+
+
+# Sync is a blocking call and it will block the execution of the program
+# Waiting for the task to complete
