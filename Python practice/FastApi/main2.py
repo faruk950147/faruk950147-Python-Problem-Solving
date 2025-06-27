cf33ccb5e0ebb44bc194ceb6155296574bc1ff29
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-27 20:11:45 +0600

    Uploaded all files

--- Python practice/FastApi/main2.py
+++ Python practice/FastApi/main2.py
@@ -1,37 +1,59 @@
-from fastapi import FastAPI, status
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI
+from enum import Enum
 import uvicorn
-import enum
 
 app = FastAPI()
 
-class Role(enum.Enum):
+class UserType(str, Enum):
     ADMIN = "admin"
     USER = "user"
     GUEST = "guest"
 
+@app.get("/userValidation/")
+async def userValidation(user_type: UserType):
+    # FastAPI self validation but you want to validate
+    # first check user_type is in UserType
+    #if user_type == UserType.ADMIN or user_type == UserType.USER or user_type == UserType.GUEST:
+    
+    if user_type not in UserType:
+        return {
+            "status": "400",
+            "user_type": "Invalid user type"
+        }
+    return {
+        "status": "200",
+        "user_type": user_type.value
+    }
 
-@app.get("/user")
-async def user(role: Role):
-    if role == Role.ADMIN:
-        return JSONResponse(content={
-            "status": "success",
-            "data": role.value
-        }, status_code=status.HTTP_200_OK)
-    elif role == Role.USER:
-        return JSONResponse(content={
-            "status": "success",
-            "data": role.value
-        }, status_code=status.HTTP_200_OK)
-    elif role == Role.GUEST:
-        return JSONResponse(content={
-            "status": "success",
-            "data": role.value
-        }, status_code=status.HTTP_200_OK)      
-    else:
-        return JSONResponse(content={
-            "status": "success",
-            "data": role.value
-        }, status_code=status.HTTP_200_OK)
+@app.get("/users")
+async def users():
+    return {
+        "users": [
+            {"id": 1, "name": "John Doe", "email": "john.doe@example.com", "user_type": UserType.ADMIN.value},
+            {"id": 2, "name": "Butler", "email": "butler@example.com", "user_type": UserType.USER.value},
+            {"id": 3, "name": "Steve", "email": "steve@example.com", "user_type": UserType.GUEST.value},
+        ]
+    }
+
+@app.get("/user_profile/{id}")
+async def profile(id: int):
+    users = [
+        {"id": 1, "name": "John Doe", "email": "john.doe@example.com", "user_type": UserType.ADMIN.value},
+        {"id": 2, "name": "Butler", "email": "butler@example.com", "user_type": UserType.USER.value},
+        {"id": 3, "name": "Steve", "email": "steve@example.com", "user_type": UserType.GUEST.value},
+    ]
+
+    for user in users:
+        if user["id"] == id:
+            return {
+                "status": "200",
+                "profile": user
+            }
+
+    return {
+        "status": "404",
+        "message": "User not found"
+    }
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
