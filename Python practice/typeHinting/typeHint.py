588517cbba79c9558bbe8f2827e65a9d035e22b8
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-27 13:00:57 +0600

    Uploaded all files

--- Python practice/typeHinting/typeHint.py
+++ Python practice/typeHinting/typeHint.py
@@ -15,17 +15,30 @@
 # if __name__ == "__main__":
 #     adder = Adder(1, 2)
 #     print(adder.add())
-    
 
-class User:
-    def __init__(self, name: str, age: int, email: str):
-        self.name = name
-        self.age = age
-        self.email = email
+# class User:
+#     def __init__(self):
+#         self.name : str = "John"
+#         self.age : int = 30
+#         self.email : str = "john@example.com"
 
-    def __str__(self) -> str:
-        return f"User(name={self.name}, age={self.age}, email={self.email})"
+#     def __str__(self) -> str:
+#         return f"User(name={self.name}, age={self.age}, email={self.email})"
 
-if __name__ == "__main__":
-    user = User("John", 30, "john@example.com")
-    print(user)
+# if __name__ == "__main__":
+#     user = User()
+#     print(user)
+
+# class User:
+#     def __init__(self, name: str, age: int, email: str):
+#         self.name = name
+#         self.age = age
+#         self.email = email
+
+#     def __str__(self) -> str:
+#         return f"User(name={self.name}, age={self.age}, email={self.email})"
+
+# if __name__ == "__main__":
+#     user = User("John", 30, "john@example.com")
+#     print(user)
+
