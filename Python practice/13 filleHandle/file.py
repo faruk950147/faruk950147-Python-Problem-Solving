f387bc45b3a07bcf198ff35b7b942fa07fd50e1e
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-27 23:18:11 +0600

    Uploaded all files

--- Python practice/13 filleHandle/file.py
+++ Python practice/13 filleHandle/file.py
@@ -1,19 +1,22 @@
-# file = open("file1.txt", "w") 
-# file.write("Hello, World!")
-# file.close()
+import os
 
-# file = open("file1.txt", "r")
+
+file = open("L:\Python/Python Problem Solving/Python practice/13 filleHandle/dataFile/file1.txt", "w") 
+file.write("Hello, World!")
+file.close()
+
+# file = open("L:\Python/Python Problem Solving/Python practice/13 filleHandle/dataFile/file1.txt", "r")
 # print(file.read())
 # file.close()
 
-# file = open("file1.txt", "r+")
+# file = open("L:\Python/Python Problem Solving/Python practice/13 filleHandle/dataFile/file1.txtt", "r+")
 # print(file.read())
 # file.write("Hello, Python!")
 # file.seek(0)
 # print(file.read())
 # file.close()
 
-# with open("file1.txt", "r+") as file:
+# with open("L:\Python/Python Problem Solving/Python practice/13 filleHandle/dataFile/file1.txt", "r+") as file:
 #     print(file.read())
 #     file.write("Hello, World!")
 #     file.seek(0)
