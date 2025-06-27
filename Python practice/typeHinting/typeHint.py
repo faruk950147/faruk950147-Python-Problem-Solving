36e9e12cc1fec47e016b5664baa1d2a04119aa04
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-27 10:56:59 +0600

    Uploaded all files

--- Python practice/typeHinting/typeHint.py
+++ Python practice/typeHinting/typeHint.py
@@ -1,15 +1,31 @@
-def add(a: int, b: int) -> int:
-    return a + b
+# def add(a: int, b: int) -> int:
+#     return a + b
 
-print(add(1, 2))
+# if __name__ == "__main__":
+#     print(add(1, 2))
 
-class Adder:
-    def __init__(self, a: int, b: int):
-        self.a = a
-        self.b = b
+# class Adder:
+#     def __init__(self, a: int, b: int):
+#         self.a = a
+#         self.b = b
 
-    def add(self) -> int:
-        return self.a + self.b
+#     def add(self) -> int:
+#         return self.a + self.b
 
-adder = Adder(1, 2)
-print(adder.add())
+# if __name__ == "__main__":
+#     adder = Adder(1, 2)
+#     print(adder.add())
+    
+
+class User:
+    def __init__(self, name: str, age: int, email: str):
+        self.name = name
+        self.age = age
+        self.email = email
+
+    def __str__(self) -> str:
+        return f"User(name={self.name}, age={self.age}, email={self.email})"
+
+if __name__ == "__main__":
+    user = User("John", 30, "john@example.com")
+    print(user)
