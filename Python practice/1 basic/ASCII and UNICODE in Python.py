4a9aee76a1352f060fcbe9b786a7ecba349387f2
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-29 08:51:10 +0600

    Uploaded all files

--- Python practice/1 basic/ASCII and UNICODE in Python.py
+++ Python practice/1 basic/ASCII and UNICODE in Python.py
@@ -3,6 +3,7 @@
 # Capital letter A to Z = 65 to 90
 # Small letter a to z = 97 to 122
 # ord() function returns the Unicode code point of a character
+# chr() function returns the character that represents the Unicode code point   
 print(ord("A"))
 print(ord("a"))
 print(ord("0"))
@@ -13,5 +14,3 @@ print(chr(97))
 print(chr(48))
 print(chr(32))
 
-# ord() function returns the Unicode code point of a character
-# chr() function returns the character that represents the Unicode code point   
