e8207ac778cd2033d48eaa093941aed83739535f
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-29 11:09:22 +0600

    Uploaded all files

---
+++ Python practice/1 basic/expression.py
@@ -0,0 +1,8 @@
+#it is a statement that returns a value
+
+# 5 + 3 = Expression (value 8)
+
+def add(a, b): # Statement (Function definition)
+    return a + b # Expression (value 8)
+
+add(5, 3) # Expression (value 8)
---
+++ Python practice/1 basic/statement.py
@@ -0,0 +1,22 @@
+#it is a statement that does not return any value
+
+
+# 5 + 3 = Expression (value 8)
+# x = 8 = Statement (Variable assignment)
+
+# if x > 5:
+#     print("x is greater than 5") # Statement (Conditional statement)
+
+# while x > 5:
+#     print("x is greater than 5") # Statement (Conditional statement)    
+
+# for i in range(5):
+#     print("x is greater than 5") # Statement (Conditional statement)
+
+
+def add(a, b): # Statement (Function definition)
+    return a + b # Expression (value 8)
+
+add(5, 3) # Expression (value 8)
+
+print(add(5, 3)) # Expression (value 8)
