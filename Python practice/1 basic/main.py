f618b0526f18e018772e9a035f9c95ee36a81799
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-29 10:36:35 +0600

    Uploaded all files

--- Python practice/1 basic/main.py
+++ Python practice/1 basic/main.py
@@ -10,3 +10,25 @@
     
 
 
+# x = 10
+# y = 20
+# print(x and y) #and two operand integer return last operand y
+# print(x or y) #or two operand integer return first operand x
+# print(not x) #not operand integer return opposite of operand x
+# print(not y) #not operand integer return opposite of operand y
+# print(not not x) #not operand integer return opposite of operand x
+# print(not not y) #not operand integer return opposite of operand y
+# print(not not not x) #not operand integer return opposite of operand x
+# print(not not not y) #not operand integer return opposite of operand y
+
+str1 = "Hello"
+str2 = "World"
+print(str1 and str2) #and two operand string return last operand str2
+print(str1 or str2) #or two operand string return first operand str1
+print(not str1) #not operand string return opposite of operand str1
+print(not str2) #not operand string return opposite of operand str2
+print(not not str1) #not operand string return opposite of operand str1
+print(not not str2) #not operand string return opposite of operand str2
+print(not not not str1) #not operand string return opposite of operand str1
+print(not not not str2) #not operand string return opposite of operand str2
+
