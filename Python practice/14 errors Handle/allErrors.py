6258c200fcf6e33453a2c2a4e09a2d6cf6837ece
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-29 10:50:19 +0600

    Uploaded all files

---
+++ Python practice/14 errors Handle/allErrors.py
@@ -0,0 +1,37 @@
+# # all errors list
+# SyntaxError
+# EOFError
+# ImportError
+# IndexError
+# KeyError
+# NameError
+# ZeroDivisionError
+# AttributeError
+# TypeError
+# ValueError
+# KeyboardInterrupt
+# RuntimeError
+# RecursionError
+# SystemError
+# SystemExit
+# ModuleNotFoundError
+# OverflowError
+# ReferenceError
+# StopAsyncIteration
+# StopIteration
+# TabError
+# UnboundLocalError
+# UnicodeError
+# UnicodeEncodeError
+# UnicodeDecodeError
+# UnicodeTranslateError
+# Warning
+
+# syntax error
+# print("Hello, World!) # missing closing quote
+# semantic error
+# a = 9
+# b = 10
+# print(a + b/3) # 2 average
+# runtime error
+# print(3/0)
