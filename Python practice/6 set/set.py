5ff56d49a6d654f445b579770328721b0f93deb3
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-29 17:22:10 +0600

    Uploaded all files

--- Python practice/6 set/set.py
+++ Python practice/6 set/set.py
@@ -1,5 +1,15 @@
-set1 = {1, 2, 3, 4, 5}
-set2 = {4, 5, 6, 7, 8}
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {4, 5, 6, 7, 8}
+set1 = {1,2,3,4,5}
+set2 = {1,2,3,4,5}
+print(set1.union(set2)) # union
+print(set1 | set2) # union
+print(set1.intersection(set2)) # intersection
+print(set1 & set2) # intersection
+print(set1.difference(set2)) # difference
+print(set1 - set2) # difference
+print(set1.symmetric_difference(set2)) # symmetric difference
+print(set1 ^ set2) # symmetric difference
 
 # print(set1)
 # print(set2)
@@ -8,7 +18,13 @@ set2 = {4, 5, 6, 7, 8}
 # print(set1.discard(2))
 # print(set1.pop())
 
-print(set1 | set2) # union
-print(set1 & set2) # intersection
-print(set1 - set2) # difference
-print(set1 ^ set2) # symmetric difference
+# set SubSet
+# print(set1.issubset(set2))
+# print(set1.issuperset(set2))
+# print(set1.isdisjoint(set2))
+# print(set1.symmetric_difference(set2))
+
+
+
+
+
