fec433ba9166a3ded80ab5d14f6660178b203465
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-29 12:07:28 +0600

    Uploaded all files

---
+++ Python practice/1 basic/Ternary Operator.py
@@ -0,0 +1,21 @@
+# Ternary Operator in Python 
+#ternary operator 3 part (condition, true, false)
+# variable = value_if_true if condition else value_if_false
+
+# a = 10
+# b = 20
+# print("a is greater than b" if a > b else "b is greater than a")
+
+# ✅ Walrus Operator (:=) in Python
+
+# Walrus Operator Python 3.8 introduce 
+# assignment and condition check one line 
+# actual name : Assignment Expression Operator
+# name "Walrus" because it's sign := looks like Walrus (a sea animal) 😄
+# walrus operator :=
+# walrus operator is used to assign value to variable as a part of a larger expression
+# it is used in while loop
+
+# a = 10
+# while (a := a - 1) > 0:
+#     print(a)
