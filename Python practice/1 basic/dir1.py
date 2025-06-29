e813a0e44c8ea0e230e1f269a52504e9a2f5842b
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-06-29 09:06:12 +0600

    Uploaded all files

--- Python practice/1 basic/dir1.py
+++ Python practice/1 basic/dir1.py
@@ -2,19 +2,29 @@
 
 # check available attributes and methods of any object
 
-# dir() function returns a list of the attributes and methods of any object
-print(dir())
+# # dir() function returns a list of the attributes and methods of any object
+# print(dir())
 
-# dir() function returns a list of the attributes and methods of any object
-print(dir("Hello"))
+# # dir() function returns a list of the attributes and methods of any object
+# print(dir("Hello"))
 
-# dir() function returns a list of the attributes and methods of any object
-print(dir(123))
+# # dir() function returns a list of the attributes and methods of any object
+# print(dir(123))
 
-# dir() function returns a list of the attributes and methods of any object
-print(dir([1, 2, 3]))
+# # dir() function returns a list of the attributes and methods of any object
+# print(dir([1, 2, 3]))
 
-# dir() function returns a list of the attributes and methods of any object
-print(dir({"a": 1, "b": 2, "c": 3}))
+# # dir() function returns a list of the attributes and methods of any object
+# print(dir({"a": 1, "b": 2, "c": 3}))
 
-    
+
+# suppose creating a module
+
+# module.py
+import random
+
+print(dir(random))
+# def add(a, b):
+#     return a + b
+
+# print(dir())
